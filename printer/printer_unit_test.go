d43aecfe133d80b629f4e6922723d8654a09e222
teleivo <teleivo@users.noreply.github.com>
2025-01-14 07:24:41 +0100

    test: add failing idempotency test

--- printer/printer_unit_test.go
+++ printer/printer_unit_test.go
@@ -41,31 +41,32 @@ bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
  boundaries several times of course as long as this is necessary it should also respect giant URLs \
 https://github.com/teleivo/dot/blob/fake/27b6dbfe4b99f67df74bfb7323e19d6c547f68fd/parser_test.go#L13"`,
 		},
-		// 	// takes the output from QuotedIDPastMaxColumnIsBrokenUp as input and output
-		// 	"BreakingUpQuotedIDIsIdempotent": {
-		// 		in: `"This is a test of a long attribute value that is past the max column which should be split on word\
-		// boundaries several times of course as long as this is necessary it should also respect giant URLs\
-		// https://github.com/teleivo/dot/blob/fake/27b6dbfe4b99f67df74bfb7323e19d6c547f68fd/parser_test.go#L13"`,
-		// 		want: `"This is a test of a long attribute value that is past the max column which should be split on word\
-		// boundaries several times of course as long as this is necessary it should also respect giant URLs\
-		// https://github.com/teleivo/dot/blob/fake/27b6dbfe4b99f67df74bfb7323e19d6c547f68fd/parser_test.go#L13"`,
-		// 	},
 		// TODO add test with split quoted ID that is split in a different place than I would, these
 		// should be stripped and \\n be placed as if the ID never had any.
 		// TODO how does my current approach deal with special characters? as whitespace is used as a
 		// word boundary
+		// TODO add idempotency test to main printer test as well
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			var got bytes.Buffer
-			p := Printer{w: &got}
-			in := ast.ID{Literal: test.in}
+			var gotFirst bytes.Buffer
+			p := Printer{w: &gotFirst}
 
-			err := p.printID(in)
+			err := p.printID(ast.ID{Literal: test.in})
 			require.NoErrorf(t, err, "printID()")
 
-			assert.EqualValuesf(t, got.String(), test.want, "printID")
+			require.EqualValuesf(t, gotFirst.String(), test.want, "printID")
+
+			t.Logf("printID should be idempotent")
+
+			var gotSecond bytes.Buffer
+			p = Printer{w: &gotSecond}
+
+			err = p.printID(ast.ID{Literal: gotFirst.String()})
+			require.NoErrorf(t, err, "printID()")
+
+			assert.EqualValuesf(t, gotSecond.String(), gotFirst.String(), "printID")
 		})
 	}
 }

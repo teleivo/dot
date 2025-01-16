2c79003fd086cf1f4a8b754aaa20004db8d3e32d
teleivo <teleivo@users.noreply.github.com>
2025-01-16 07:36:31 +0100

    test: failing test

--- printer/printer_unit_test.go
+++ printer/printer_unit_test.go
@@ -41,6 +41,20 @@ bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
  boundaries several times of course as long as this is necessary it should also respect giant URLs \
 https://github.com/teleivo/dot/blob/fake/27b6dbfe4b99f67df74bfb7323e19d6c547f68fd/parser_test.go#L13"`,
 		},
+		// TODO how hard / complex would it make my code to support newlines without line continuation?
+		// TODO add test with more than one whitespace. for some reason two whitespaces after word lead
+		// to an off by one error
+		"QuotedIDPastMaxColumnWithMultipleWhitespaces": {
+			in: `"This is a test of a long attribute value that is past the max column which should be
+			split on word  boundaries several times of course as long as this is necessary it should also
+			respect giant URLs
+			https://github.com/teleivo/dot/blob/fake/27b6dbfe4b99f67df74bfb7323e19d6c547f68fd/parser_test.go#L13"`,
+			want: `"This is a test of a long attribute value that is past the max column which should be split on word\
+  boundaries several times of course as long as this is necessary it should also respect giant URLs \
+https://github.com/teleivo/dot/blob/fake/27b6dbfe4b99f67df74bfb7323e19d6c547f68fd/parser_test.go#L13"`,
+		},
+		// TODO add test with pure newlines in the correct spot	where a line continuation should go
+		// these should be normalized
 		// input uses the same text as in QuotedIDPastMaxColumnIsBrokenUp with line continuations in
 		// places they should not be i.e. too early and too late
 		"QuotedIDWithOutOfPlaceLineContinuations": {
